b56787017bfec4c68306f72a4836d00abd229a7a
pmahesha.hera@gmail.com <pmahesha.hera@gmail.com>
2018-06-24 21:57:27 +0530

    blpn ykuu

--- backupme.sh
+++ backupme.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-from=$1 # first input
-to=$2  # second input
-cd $from #open first input folder
-cp -r * $to # copy files to second input folder
-cp $to #open second input folder
-DATE="$(date +'%Y-%m-%d')" #date
-for x in $(ls) #run amount of files in the folder
-do
-	a=$x
-	mv ./$a ./backedup_$a'_'DATE.bac
-done
+printf("Dilanka + Ridmi");
